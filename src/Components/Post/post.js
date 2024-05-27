a1665ccd130a892d305dbad29b97b4851e5311ea
kebeattie <kebeattie98@hotmail.com>
2024-05-27 16:59:35 +0100

    E.O.D 27/05/2024

--- src/Components/Post/post.js
+++ src/Components/Post/post.js
@@ -17,6 +17,17 @@ export function Post() {
                     <span><p>Posted by: lfcfella2k7</p></span><span className={styles.comment}><img className={styles.commentIcon} src={comment}/>123</span>
                 </div>
             </div>
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <h5>After 8 years, Jurgen Klopp leaves Liverpool.</h5>
+                </div>
+                <div className={styles.content}>
+                    <img src={klopp}/>
+                </div>
+                <div className={styles.metaData}>
+                    <span><p>Posted by: lfcfella2k7</p></span><span className={styles.comment}><img className={styles.commentIcon} src={comment}/>123</span>
+                </div>
+            </div>
         </>
     )
 }
