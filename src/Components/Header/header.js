cb61e5feb3959241c9e4b0362939c9bae43dc90d
kebeattie <kebeattie98@hotmail.com>
2024-05-27 14:37:37 +0100

    Added Header - No search bar functionality for now

---
+++ src/Components/Header/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from './header.module.css'
+import file from '../../media/file.png'
+
+export function Header () {
+    return (
+        <>
+            <header>
+                <div className={styles.logoAndTitle}>
+                    <img className={styles.eplLogo} src={file} />
+                    <h1 className={styles.title}>Reddit<span className={styles.eplText}>EPL</span></h1>
+                </div>
+                <form>
+                    <input className={styles.searchBar} type='text' placeholder="Search"/>
+                </form>
+
+            </header>
+        </>
+    )
+}
